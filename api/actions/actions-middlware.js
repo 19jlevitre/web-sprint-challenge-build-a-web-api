4a7e131f4a48e3067cb0512854c229bd09f756eb
Jonathon LeVitre <jonathonlevitre@Jonathons-MacBook-Pro.local>
2021-10-29 12:50:32 -0600

    all get functions working in action router

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,19 @@
 // add middlewares here related to actions
+const Action = require('./actions-model');
+
+function checkActionId(req, res, next) {
+    Action.get(req.params.id)
+    .then(possibleAction => {
+        if (possibleAction) {
+            req.actionFromDb = possibleAction
+            next()
+        } else {
+            next({ status: 404,message: "action not found"})
+        }
+    })
+    .catch(next)
+}
+
+module.exports = {
+    checkActionId,
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,64 @@
-// Write your "actions" router here!
+const express = require('express');
+const Action = require('./actions-model.js');
+const router = express.Router();
+const {
+    checkActionId,
+} = require('./actions-middlware');
+router.get('/', (req, res) => {
+    Action.get(req.params.id)
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(error => {
+        console.log(error)
+    });
+});
+
+router.get('/:id', checkActionId, (req, res) => {
+    res.status(200).json(req.actionFromDb)
+});
+
+// router.post('/', validateProject, (req, res, next) => {
+//     Project.insert(req.body)
+//     .then(newProject => {
+//         res.status(201).json(newProject)
+//     })
+
+// });
+
+// router.put('/:id', checkProjectId, validateProject, (req, res, next) => {
+   
+   
+//     Project.update(req.params.id, req.body)
+//         .then(updatedProject => {
+//             res.json(updatedProject)         
+//         }).catch(err => {
+//         console.log(err)
+//         next()
+//     })
+  
+// });
+
+// router.delete('/:id', checkProjectId, (req, res, next) => {
+//     Project.remove(req.params.id)
+//     .then(() => {
+//         res.status(200).json(req.projectFromDbprojectFromDb)
+//     }).catch(next)
+
+// });
+
+// router.get('/:id/actions', (req, res, next)=> {
+//     Project.getProjectActions(req.params.id)
+//     .then(actions => {
+//         res.json(actions)
+//     }).catch(err => {
+//         console.log(err)
+//     })
+
+// });
+
+
+
+
+
+module.exports = router
